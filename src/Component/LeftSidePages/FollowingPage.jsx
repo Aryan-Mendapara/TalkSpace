066dcd7ce17dae307024d8c027dc8812aeaeb973
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-05-01 18:09:31 +0530

    first commit

--- src/Component/LeftSidePages/FollowingPage.jsx
+++ src/Component/LeftSidePages/FollowingPage.jsx
@@ -1,9 +1,13 @@
 
 const FollowingPage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="flex flex-col p-6">
+        <div className="max-w-4xl mx-auto">
+          <h1 className="text-3xl text-center font-bold mb-6">People You're Following</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
