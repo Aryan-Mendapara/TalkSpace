7329176670efb27fc85ece61a5259acc21bcdc7a
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-05-08 17:25:52 +0530

    first commit

--- src/Component/LeftSidePages/FollowingPage.jsx
+++ src/Component/LeftSidePages/FollowingPage.jsx
@@ -2,7 +2,7 @@
 const FollowingPage = () => {
   return (
     <>
-      <div className="flex flex-col p-6">
+      <div className="ml-50 mt-5">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-3xl text-center font-bold mb-6">People You're Following</h1>
         </div>
--- src/Component/LeftSidePages/SavePage.jsx
+++ src/Component/LeftSidePages/SavePage.jsx
@@ -24,50 +24,60 @@ const SavePage = () => {
   };
 
   return (
-    <div className="flex justify-center">
-      <main>
-        <div className="text-center ">
-          <h2 className="text-2xl font-bold text-gray-900 border-b pb-2">
-            Your Saved Posts
-          </h2>
-        </div>
+    <div className="ml-60">
+      <div className="justify-center items-center">
+        <h2 className="text-2xl font-bold text-gray-900 border-b pb-2">
+          Your Saved Posts
+        </h2>
+      </div>
 
-        {/* Saved Posts */}
-        <div>
-          {savePosts.length === 0 ? (
-            <div className="flex flex-col items-center text-gray-500 mt-10">
-              <img
-                src="/assest/no-saved-posts.png"
-                alt="No saved"
-                className="w-52 mb-4"
-              />
-              <p>You haven't saved any posts yet.</p>
-            </div>
-          ) : (
-            savePosts.map((post) => {
-              <div
-                key={post.id}
-                className="relative bg-white p-6 rounded-lg shadow-lg border border-gray-200 mb-6"
+      {/* Saved Posts */}
+      <div>
+        {savePosts.length === 0 ? (
+          <div className="items-center text-gray-500 mt-10">
+            <img
+              src="/assest/no-saved-posts.png"
+              alt="No saved"
+              className="w-52 mb-4"
+            />
+            <p>You haven't saved any posts yet.</p>
+          </div>
+        ) : (
+          savePosts.map((post) => {
+            <div
+              key={post.id}
+              className="relative bg-white p-6 rounded-lg shadow-lg border border-gray-200 mb-6"
+            >
+              {/* Unsave Button */}
+              <button
+                onClick={() => handleUnsave(post.id)}
+                className="absolute top-4 rigth-4 text-gray-400 hover:text-red-500"
+                title="Unsave"
               >
-                {/* Unsave Button */}
-                <button
-                  onClick={() => handleUnsave(post.id)}
-                  className="absolute top-4 rigth-4 text-gray-400 hover:text-red-500"
-                  title="Unsave"
-                >
-                  <FaBookmark className="text-xl" />
-                </button>
+                <FaBookmark className="text-xl" />
+              </button>
 
-                {/* User Info */}
-                <div className="flex items-center gap-3 mb-3">
-                  <div className="bg-gray-300 w-10 h-10 rounded-full"></div>
-                  <div></div>
+              {/* User Info */}
+              <div className="flex items-center gap-3 mb-3">
+                <div className="bg-gray-300 w-10 h-10 rounded-full"></div>
+                <div>
+                  <p className="text-sm text-gray-400">
+                    {new Data(post.createdAt).toLocaleString()}
+                  </p>
                 </div>
               </div>
-            })
-          )}
-        </div>
-      </main>
+
+              {/* Post Content */}
+              <h3 className="text-lg font-bold text-gray-900">
+                {post.title}
+              </h3>
+              <p className="text-gray-700 mt-2">{post.content}</p>
+
+              {/* Like and Comment Section */}
+            </div>
+          })
+        )}
+      </div>
     </div>
   )
 }
